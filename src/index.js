606c974a692d338d57f6be9d6dcd77c1c587e1bc
Cho yoon ho <98027471+dydgh142@users.noreply.github.com>
2025-02-05 15:44:19 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import MotorSpeedQuiz from "./components/MotorSpeedQuiz"; // 올바른 경로 확인
+import App from "./App"; 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <MotorSpeedQuiz />
+    <App />
   </React.StrictMode>
 );
