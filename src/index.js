86ebc185c9c88fd7aa351183d53c69daa63d8548
Cho yoon ho <98027471+dydgh142@users.noreply.github.com>
2025-02-05 15:49:11 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App"; 
+import App from "./App";
+import "./index.css"; // Tailwind 스타일 적용
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
