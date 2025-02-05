05c3ccf19afe83d63eaa3e47c52fdd55d2b9f519
Cho yoon ho <98027471+dydgh142@users.noreply.github.com>
2025-02-05 15:57:00 +0900

    Update Button.js

--- src/components/Button.js
+++ src/components/Button.js
@@ -6,4 +6,7 @@ export function Button({ children, className, ...props }) {
       className={`px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 ${className}`}
       {...props}
     >
-     
+      {children}
+    </button>
+  );
+}
